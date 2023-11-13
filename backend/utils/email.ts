b38553ea8b681d28769d2a985b0a65409d6fe325
Edison Liem <edisonliem417@gmail.com>
2023-11-12 22:15:41 -0500

    Update email template's discord link

--- backend/utils/email.ts
+++ backend/utils/email.ts
@@ -1,7 +1,7 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export const memberEmailTemplate = (name: string) => {
-    return `
+  return `
         Hello ${name},
         <br><br>
         Thank you for signing up for our membership!
@@ -11,36 +11,38 @@ export const memberEmailTemplate = (name: string) => {
         support from our club and be the first ones to know about what's going on in the
         FinTech industry.
         <br><br>
-        Discord Link: <a href='https://discord.gg/mQSswsFzuy'>https://discord.gg/mQSswsFzuy</a>
+        Discord Link: <a href='https://discord.gg/ANEnxS7up4'>https://discord.gg/ANEnxS7up4</a>
         <br><br>
         Sincerely,<br>
         UTSC FinTech Society
-    `
+    `;
 };
 
 const sendEmail = (to: string, subject: string, html: string) => {
-    const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: to,
-        subject: subject,
-        html: html
-    };
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: subject,
+    html: html,
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(`[nodemailer]: Error: ${error}`);
-        } else {
-            console.info(`[nodemailer]: Email <to: ${mailOptions.to}, subject: ${mailOptions.subject}> sent: ${info.response}`);
-        }
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(`[nodemailer]: Error: ${error}`);
+    } else {
+      console.info(
+        `[nodemailer]: Email <to: ${mailOptions.to}, subject: ${mailOptions.subject}> sent: ${info.response}`
+      );
+    }
+  });
 };
 
 export default sendEmail;
